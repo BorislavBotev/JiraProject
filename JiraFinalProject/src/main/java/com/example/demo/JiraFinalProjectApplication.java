9089ba44f997a01772d0948ec0de0303ef3464b6
Milen Valchev <milen.valchev13@gmail.com>
2019-03-02 14:01:07 +0200

    Initial Commit

---
+++ JiraFinalProject/src/main/java/com/example/demo/JiraFinalProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiraFinalProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JiraFinalProjectApplication.class, args);
+	}
+
+}
