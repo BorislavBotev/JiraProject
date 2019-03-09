74359f48f48f2f179fc2e31a647820e5d2894974
Milen Valchev <milen.valchev13@gmail.com>
2019-03-09 20:45:38 +0200

    Worklog functionality methods

--- JiraFinalProject/src/main/java/com/example/demo/service/WorklogService.java
+++ JiraFinalProject/src/main/java/com/example/demo/service/WorklogService.java
@@ -1,6 +1,8 @@
 package com.example.demo.service;
 
+import java.time.Duration;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -8,12 +10,14 @@ import org.springframework.stereotype.Service;
 import com.example.demo.dto.AddWorklogDTO;
 import com.example.demo.exceptions.IssueException;
 import com.example.demo.exceptions.ProjectException;
+import com.example.demo.exceptions.UserException;
 import com.example.demo.exceptions.WorklogException;
 import com.example.demo.model.Issue;
 import com.example.demo.model.User;
 import com.example.demo.model.Worklog;
 import com.example.demo.repositories.IssueRepository;
 import com.example.demo.repositories.ProjectRepository;
+import com.example.demo.repositories.UserRepository;
 import com.example.demo.repositories.WorklogRepository;
 
 @Service
@@ -25,6 +29,9 @@ public class WorklogService {
 	private IssueRepository issueRepository;
 	@Autowired
 	private ProjectRepository projectRepository;
+	@Autowired 
+	private UserRepository userRepository;
+	
 	
 	public void addWorklog(User user, AddWorklogDTO worklogDTO, long projectId, long issueId) throws ProjectException, IssueException, WorklogException {
 		if(!projectRepository.findById(projectId).isPresent()) {
@@ -43,5 +50,52 @@ public class WorklogService {
 		Worklog worklog = new Worklog(user, issue, description, hoursSpent, hoursRemaining);
 		worklogRepository.save(worklog);
 	}
-
+	
+	
+	public List<WorklogDTO> getWorklogOfProject(long projectId) throws ProjectException {
+		if(!projectRepository.findById(projectId).isPresent()) {
+			throw new ProjectException("Project Not Found!");
+		}
+		return worklogRepository.findAll().stream()
+				.filter(wl -> wl.getIssue().getProject().getId() == projectId)
+				.map(wl -> new WorklogDTO(wl.getId(), wl.getUser().getUsername(), wl.getIssue().getSummary(),
+							wl.getDescription(), wl.getHoursSpent(), wl.getHoursRemaining(), wl.getLogDateTime()))
+				.collect(Collectors.toList());
 	}
+	
+	
+	public List<WorklogDTO> getWorklogOfIssue(long projectId, long issueId) throws ProjectException, IssueException {
+		if(!projectRepository.findById(projectId).isPresent()) {
+			throw new ProjectException("Project Not Found!");
+		}
+		if(!issueRepository.findById(issueId).isPresent()) {
+			throw new IssueException("Issue Not Found!");
+		}
+		
+		return worklogRepository.findAll().stream()
+				.filter(wl -> wl.getIssue().getId() == issueId)
+				.map(wl -> new WorklogDTO(wl.getId(), wl.getUser().getUsername(), wl.getIssue().getSummary(),
+							wl.getDescription(), wl.getHoursSpent(), wl.getHoursRemaining(), wl.getLogDateTime()))
+				.collect(Collectors.toList());
+	}
+	
+	
+	public List<WorklogDTO> getWorklogOfCurrentUser(long userId) {
+		return worklogRepository.findAll().stream()
+				.filter(wl -> wl.getUser().getId() == userId)
+				.map(wl -> new WorklogDTO(wl.getId(), wl.getUser().getUsername(), wl.getIssue().getSummary(),
+							wl.getDescription(), wl.getHoursSpent(), wl.getHoursRemaining(), wl.getLogDateTime()))
+				.collect(Collectors.toList());
+	}
+	
+	
+	
+	
+	
+	public void deleteWorklog(long worklogId) throws WorklogException {
+		if(!worklogRepository.findById(worklogId).isPresent()) {
+			throw new WorklogException("Worklog Not Found!");
+		}
+		worklogRepository.deleteById(worklogId);
+	}
+}
